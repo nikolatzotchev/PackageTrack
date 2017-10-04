487682f3080b74634837cc1d5b42ce8441d0b187
Valentin Valchev <v.valchev@ocado.com>
2017-10-04 19:22:02 +0300

    Добавен е Swagger-UI за по-лесно тестване на REST API. Виж http://localhost:8080/swagger-ui.html след пускане на приложението.

--- Server/src/main/java/com/begin/Application.java
+++ Server/src/main/java/com/begin/Application.java
@@ -1,13 +1,50 @@
 package com.begin;
 
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
+import com.fasterxml.classmate.TypeResolver;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.WildcardType;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+
+  @Autowired
+  private TypeResolver typeResolver;
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public Docket petApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.regex("/api/.*"))
+        .build()
+        .pathMapping("/")
+        .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
+        .genericModelSubstitutes(ResponseEntity.class)
+        .alternateTypeRules(
+            newRule(typeResolver.resolve(DeferredResult.class,
+                typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+                typeResolver.resolve(WildcardType.class)))
+        .useDefaultResponseMessages(true)
+        .enableUrlTemplating(false)
+        ;
+  }
+
 }
