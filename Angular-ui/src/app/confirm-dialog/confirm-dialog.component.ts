78a93d175f27880496bf2fc6d505352680bd2a25
NIkola Tzotchev (Никола Цочев) <ntzotchev@gmail.com>
2017-10-28 20:43:28 +0300

    Create confirm-dialog that will be use for every confirmation needed in the app.

---
+++ Angular-ui/src/app/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Inject} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.css']
+})
+export class ConfirmDialogComponent implements OnInit {
+
+  ngOnInit() {
+  }
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  confirmDeletion() {
+    this.dialogRef.close(true);
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close(false);
+  }
+
+}
